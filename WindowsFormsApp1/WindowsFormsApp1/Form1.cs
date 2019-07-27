d0ad21bd8234379220979c70af3a69c8d4fb79f5
sika <36564217+sika0115@users.noreply.github.com>
2019-07-28 03:13:27 +0900

    描画できないフォームができた

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        static int r = 220;
+        static int g = 20;
+        static int b = 60;
         public Form1()
         {
             InitializeComponent();
@@ -22,38 +25,82 @@ namespace WindowsFormsApp1
             button4.BackColor = Color.FromArgb(20, 219, 179);
             button5.BackColor = Color.FromArgb(20, 60, 219);
             button6.BackColor = Color.FromArgb(179, 20, 219);
-
-
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            
+            r = 220;
+            g = 20;
+            b = 60;
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            
+            r = 219;
+            g = 179;
+            b = 20;
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            
+            r = 60;
+            g = 219;
+            b = 20;
         }
-
         private void Button4_Click(object sender, EventArgs e)
         {
-            
+            r = 20;
+            g = 219;
+            b = 179;
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
-            
+            r = 20;
+            g = 60;
+            b = 219;
         }
 
         private void Button6_Click(object sender, EventArgs e)
         {
-            
+            r = 179;
+            g = 20;
+            b = 219;
+        }
+
+        bool State = false;  //描画状態の制御
+        int x;
+        int y;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void From1_MouseDowm(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            State = true;
+            x = e.Location.X;
+            y = e.Location.Y;
+        }
+
+        private void Form1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            State = false;
+        }
+
+        private void From1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (State)
+            {
+                Pen pen = new Pen(System.Drawing.Color.FromArgb(r,g,b), 9);
+                Graphics graphics = this.CreateGraphics();
+                graphics.DrawLine(pen, x, y, e.Location.X, e.Location.Y);
+                graphics.FillEllipse(Brushes.Black, x - pen.Width / 2, y - pen.Width / 2, pen.Width, pen.Width);
+                x = e.Location.X;
+                y = e.Location.Y;
+                pen.Dispose();
+                pen.Dispose();
+            }
         }
     }
 }
