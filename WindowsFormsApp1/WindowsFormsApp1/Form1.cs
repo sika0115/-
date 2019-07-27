3b9386a87ac7361313886e04a660ac818b60ec8c
sika <36564217+sika0115@users.noreply.github.com>
2019-07-28 00:12:52 +0900

    色のついたボタンを表示するところまで

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //ボタンの背景色設定
+            button1.BackColor = Color.FromArgb(220, 20, 60);
+            button2.BackColor = Color.FromArgb(219, 179, 20);
+            button3.BackColor = Color.FromArgb(60, 219, 20);
+            button4.BackColor = Color.FromArgb(20, 219, 179);
+            button5.BackColor = Color.FromArgb(20, 60, 219);
+            button6.BackColor = Color.FromArgb(179, 20, 219);
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
