2f2a2feedd4b6dce15a757d60f4c0383a903d663
sika <36564217+sika0115@users.noreply.github.com>
2019-07-31 11:43:27 +0900

    作り直した

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(688, 454);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            
+
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(106, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 36);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 36);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 36);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 347);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 36);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(25, 154);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 36);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(25, 96);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 36);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        float x1;
+        float y1;
+        float x2;
+        float y2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.BackColor = Color.Black;
+            button7.BackColor = Color.Red;
+            button6.BackColor = Color.Blue;
+            button2.BackColor = Color.Green;
+            button4.BackColor = Color.Yellow;
+            button5.BackColor = Color.Magenta;
+        }
+        Pen pen = new Pen(Color.Black, 4);
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Black, 4);
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Red, 4);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Blue, 4);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Green, 4);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Yellow, 4);
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Magenta, 4);
+        }
+        private void PictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            x1 = System.Windows.Forms.Cursor.Position.X;//最初のX座標取得
+            y1 = System.Windows.Forms.Cursor.Position.Y;//最初のY座標取得
+        }
+        private void PictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e) 
+        {
+            x2 = System.Windows.Forms.Cursor.Position.X;//離したところのX座標取得
+            y2 = System.Windows.Forms.Cursor.Position.Y;//離したところのY座標取得
+
+            Graphics g = this.CreateGraphics();
+            PointF p1 = new PointF(x1, y1);
+            PointF p2 = new PointF(x2, y2);
+            g.DrawLine(pen, p1, p2);
+
+
+
+        }
+    }
+}
